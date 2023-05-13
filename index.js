53767315e7310f0748f6a25018d9114bcb77cc7c
Anderson Lucas <andinlucas8@gmail.com>
2023-05-13 14:47:53 -0300

    alterando o vercel

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import connectDatabase from './src/database/db.js'
 import cors from 'cors'
 import dotenv from "dotenv"
 
+
 dotenv.config()
 
 const app = express()
@@ -13,6 +14,15 @@ connectDatabase()
 
 app.use(express.json())
 app.use('/list', Route)
+
+Route.get('/', (req, res) => {
+    return res.json({
+        sucess: true,
+        message: "sucesso"
+    })
+    
+})
+
 app.use(cors())
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
