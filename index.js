36dd6700b7ccd57337d7776481f8bd6094e39412
Anderson Lucas <andinlucas8@gmail.com>
2023-05-13 21:34:05 -0300

    modificações no cors

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 import express from 'express'
 import Route from "./src/routes/route.js"
 import connectDatabase from './src/database/db.js'
+import bodyParser from 'body-parser'
 import cors from 'cors'
 import dotenv from "dotenv"
-
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
 
 const app = express()
 const port = 3000;
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(cors())
 connectDatabase()
 
+app.use(cookieParser())
 app.use(express.json())
-app.use('/list', Route)
+app.use('/', Route)
 
-Route.get('/', (req, res) => {
-    return res.json({
-        sucess: true,
-        message: "sucesso"
-    })
-    
-})
-
-app.use(cors())
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
--- src/routes/route.js
+++ src/routes/route.js
@@ -4,6 +4,7 @@ import { verificaActive, verificaId } from '../middlewares/global.middlewares.js
 
 const route = Router()
 
+
 route.post("/", cadastro)
 route.get("/", findAll)
 route.get("/:id", verificaId, verificaActive, findbyId)
